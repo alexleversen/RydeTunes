811b41fa8707a4a8acb9918edfed985fdd50ec4a
Alex Leversen <adleversen@sep.com>
2018-07-28 17:05:15 -0400

    Fixed issue with Android QR code scanner.

---
+++ RydeTunes.Android/QRCodeScanner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using RydeTunes.Droid;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+[assembly:Dependency(typeof(QRCodeScanner))]
+namespace RydeTunes.Droid
+{
+    public class QRCodeScanner : IQRCodeScanner
+    {
+
+        public async Task<string> ScanAsync()
+        {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan(Android.App.Application.Context, new MobileBarcodeScanningOptions());
+            return result?.Text ?? "";
+        }
+    }
+}
---
+++ RydeTunes.iOS/QRCodeScanner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using RydeTunes.iOS;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+[assembly:Dependency (typeof(QRCodeScanner))]
+namespace RydeTunes.iOS
+{
+    public class QRCodeScanner : IQRCodeScanner
+    {
+        public QRCodeScanner()
+        {
+        }
+
+        public async Task<string> ScanAsync() {
+            var scanner = new MobileBarcodeScanner();
+            var result = await scanner.Scan(new MobileBarcodeScanningOptions());
+            return result?.Text ?? "";
+        }
+    }
+}
