22ddf57742fffe4d4a18c1b6a717085d2aea8677
Luke M. Pell <lmpell@net.sep.com>
2018-07-28 17:08:14 -0400

    Adding functionallity for Spotify API

--- RydeTunes/Network/SpotifyApi.cs
+++ RydeTunes/Network/SpotifyApi.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace RydeTunes.Network
@@ -8,21 +10,71 @@ namespace RydeTunes.Network
     {
         public static SpotifyApi Instance;
 
-        public event EventHandler SessionInvalidated;
+        private static string SPOTIFY_API_URL = "https://api.spotify.com";
 
-        public bool CheckSpotifySession()
+        private HttpClient spotifyClient;
+
+
+        private SpotifyApi(string authToken)
         {
-            return true; //false if playlist is empty AND a song has been added to it this session
+            UpdateToken(authToken);
+            Instance = this;
+        }
+        public void UpdateToken(string authToken)
+        {
+            if (string.IsNullOrEmpty(authToken))
+            {
+                throw new Exception("This Value can't be empty");
+            }
+            spotifyClient = new HttpClient();
+
+            spotifyClient.BaseAddress = new Uri(SPOTIFY_API_URL);
+            spotifyClient.DefaultRequestHeaders.Accept.Clear();
+            spotifyClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authToken);
         }
 
-        public bool PlaylistIsEmpty()
+        public event EventHandler SessionInvalidated;
+
+        /* Driver methods */
+
+        public string GetRydeTunesPlaylist()
+        {
+            // Get playlist if it exists
+            
+            // Clear playlist
+            // Return id
+            return "";
+        }
+        // Searches for playlists of the current user with the given name
+        public async string SearchForPlaylist(string playlistName)
+        {
+            HttpResponseMessage response = await spotifyClient.GetAsync("v1/me/playlists");
+            var values = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(await response.Content.ReadAsStringAsync());
+            foreach (Dictionary<string, string> i in values["items"])
+            {
+
+            }
+
+        }
+
+        public bool PlaylistIsEmpty(string playlistId)
         {
             return true; //TODO: Implement
         }
 
-        public void DisconnectFromPlaylist()
+        public void ClearPlaylist(string playlistId)
         {
             //TODO: Implement
         }
+
+        /* Passenger methods */
+
+        public List<String> SearchForSong(string searchTerms)
+        {
+            // TODO
+            return new List<string>();
+        }
+        
+
     }
 }
