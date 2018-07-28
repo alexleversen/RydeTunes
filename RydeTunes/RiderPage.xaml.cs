a84f0859ed3aa327cdee090f3bf9c02e5951331f
Alex Leversen <adleversen@sep.com>
2018-07-28 16:29:25 -0400

    Got QR code scanner working, added iOS provisioning profile info.

--- RydeTunes/RiderPage.xaml.cs
+++ RydeTunes/RiderPage.xaml.cs
@@ -6,15 +6,29 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Mobile;
 
 namespace RydeTunes
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RiderPage : ContentPage
 	{
+        private string _qrCodeText;
+
 		public RiderPage ()
 		{
 			InitializeComponent ();
 		}
+
+        async void ScanQRCode(object sender, System.EventArgs e) {
+            var scanner = new MobileBarcodeScanner()
+            {
+                TopText = "Test1",
+                BottomText = "Test2"
+            };
+            var scanResult = await scanner.Scan(new MobileBarcodeScanningOptions());
+            _qrCodeText = scanResult.Text;
+            qrCodeButton.Text = _qrCodeText;
+        }
 	}
 }
